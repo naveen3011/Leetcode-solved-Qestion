1c16299914eedf485876cf9fd6edb634c67c7792
Naveen kushwaha <naveenkushwaha000@gmail.com>
2023-03-27 00:44:12 +0530

    Time: 33 ms, Memory: 20.2 MB - LeetHub

---
+++ single-number/single-number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+    unordered_map<int,int> mp;
+        int n=nums.size();
+        for(int i=0 ;i<n ;i++ ){
+            mp[nums[i]]++;
+        }
+        int ans=0;
+        for(auto i : mp){
+            if(i.second==1){
+                ans=i.first;
+            }
+        }
+        return ans;
+    }
+};
