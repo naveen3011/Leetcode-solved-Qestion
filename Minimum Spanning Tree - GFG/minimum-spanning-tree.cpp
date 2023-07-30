b8b07ea63cd03a5bf9085c7c16927700e50f366a
Naveen kushwaha <naveenkushwaha000@gmail.com>
2023-07-31 01:12:13 +0530

    Added solution - LeetHub

--- Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
+++ Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
@@ -3,39 +3,124 @@
 using namespace std;
 
 // } Driver Code Ends
+
+class DisjointSet {
+    vector<int> rank, parent, size;
+public:
+    DisjointSet(int n) {
+        rank.resize(n + 1, 0);
+        parent.resize(n + 1);
+        size.resize(n + 1);
+        for (int i = 0; i <= n; i++) {
+            parent[i] = i;
+            size[i] = 1;
+        }
+    }
+
+    int findUPar(int node) {
+        if (node == parent[node])
+            return node;
+        return parent[node] = findUPar(parent[node]);
+    }
+
+    void unionByRank(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (rank[ulp_u] < rank[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+        }
+        else if (rank[ulp_v] < rank[ulp_u]) {
+            parent[ulp_v] = ulp_u;
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            rank[ulp_u]++;
+        }
+    }
+
+    void unionBySize(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (size[ulp_u] < size[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+    }
+};
+
 class Solution
 {
 	public:
-	//Function to find sum of weights of edges of the Minimum Spanning Tree.
-    int spanningTree(int V, vector<vector<int>> adj[])
-    {
-        // code here
-        priority_queue<pair<int,int> ,vector<pair<int,int>> , greater<pair<int,int>>> pq;
-        vector<int> vis(V,0);
-        pq.push({0,0});
-        int sum=0;
-        while(!pq.empty()){
-            auto it =pq.top();
-            pq.pop();
-            int node=it.second;
-            int wt=it.first;
-            if(vis[node]==1) continue;
-            sum+=wt;
-            vis[node]=1;
-            for(auto it : adj[node]){
-                int ed=it[0];
-                int wadj=it[1];
-                if(!vis[ed]){
-                    
-                    pq.push({wadj,ed});
+int spanningTree(int n, vector<vector<int>> adj[])
+{
+	// Write your code here.
+	 vector<pair<int, pair<int, int>>> edges;
+        for (int i = 0; i < n; i++) {
+            for (auto it : adj[i]) {
+                int adjNode = it[0];
+                int wt = it[1];
+                int node = i;
 
-                    
-                }
+                edges.push_back({wt, {node, adjNode}});
             }
         }
-        return sum;
-    }
+		DisjointSet ds(n);
+		sort(edges.begin(),edges.end());
+		int sumWt=0;
+		for(auto it : edges){
+			int wt=it.first;
+			int u=it.second.first;
+			int v=it.second.second;
+			if(ds.findUPar(u)!=ds.findUPar(v)){
+				ds.unionBySize(u,v);
+				sumWt+=wt;
+
+			}
+		}
+		return sumWt;
+
+}
 };
+
+// class Solution
+// {
+// 	public:
+// 	//Function to find sum of weights of edges of the Minimum Spanning Tree.
+//     int spanningTree(int V, vector<vector<int>> adj[])
+//     {
+//         // code here
+//         priority_queue<pair<int,int> ,vector<pair<int,int>> , greater<pair<int,int>>> pq;
+//         vector<int> vis(V,0);
+//         pq.push({0,0});
+//         int sum=0;
+//         while(!pq.empty()){
+//             auto it =pq.top();
+//             pq.pop();
+//             int node=it.second;
+//             int wt=it.first;
+//             if(vis[node]==1) continue;
+//             sum+=wt;
+//             vis[node]=1;
+//             for(auto it : adj[node]){
+//                 int ed=it[0];
+//                 int wadj=it[1];
+//                 if(!vis[ed]){
+                    
+//                     pq.push({wadj,ed});
+
+                    
+//                 }
+//             }
+//         }
+//         return sum;
+//     }
+// };
 
 //{ Driver Code Starts.
 
