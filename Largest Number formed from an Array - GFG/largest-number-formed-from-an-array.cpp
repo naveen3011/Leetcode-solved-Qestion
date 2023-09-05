22ea2516623e15b8dc4f791885538334030102da
Naveen kushwaha <naveenkushwaha000@gmail.com>
2023-09-06 00:13:51 +0530

    Added solution - LeetHub

--- Largest Number formed from an Array - GFG/largest-number-formed-from-an-array.cpp
+++ Largest Number formed from an Array - GFG/largest-number-formed-from-an-array.cpp
@@ -11,18 +11,22 @@ public:
 	// The main function that returns the arrangement with the largest value as
 	// string.
 	// The function accepts a vector of strings
-static	bool cpm(string a , string b){
-	    if(a+b>b+a) return true;
+	static bool cmp(string a, string b){
+	    if(a+b> b+a) return true;
 	    return false;
 	}
 	string printLargest(vector<string> &arr) {
+	    
 	    // code here
-	  sort(arr.begin(),arr.end(),cpm);
-	  string ans;
-	  for(auto it : arr){
-	      ans+=it;
-	  }
-	  return ans;
+	  
+	    
+	    sort(arr.begin(), arr.end(),cmp);
+	     string ans="";
+	    for(auto it : arr){
+	        ans+=it;
+	    }
+	    return ans;
+	    
 	}
 };
 
