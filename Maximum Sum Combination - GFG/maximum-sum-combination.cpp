fbb105368c5fd2e93f446950d18a3ec0809f3cf1
Naveen kushwaha <naveenkushwaha000@gmail.com>
2023-09-25 20:31:08 +0530

    Added solution - LeetHub

---
+++ Maximum Sum Combination - GFG/maximum-sum-combination.cpp
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    vector<int> maxCombinations(int N, int K, vector<int> &A, vector<int> &B) {
+        // code here
+         vector<int> ans;
+        priority_queue<pair<int,pair<int,int>>> pq;
+        sort(A.begin(),A.end());
+        sort(B.begin(),B.end());
+        pq.push({A[N-1]+B[N-1],{N-1,N-1}});
+        set<pair<int,int>> s;
+        s.insert({N-1,N-1});
+        while(K--){
+            pair<int,pair<int,int>> p=pq.top();
+            pq.pop();
+            ans.push_back(p.first);
+            int i=p.second.first;
+            int j=p.second.second;
+            if(i-1>=0 && s.find({i-1,j})==s.end()){
+                pq.push({A[i-1]+B[j],{i-1,j}});
+                s.insert({i-1,j});
+            }
+            if(j-1>=0 && s.find({i,j-1})==s.end()){
+                pq.push({A[i]+B[j-1],{i,j-1}});
+                s.insert({i,j-1});
+            }
+        }
+        return ans;
+
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N, K;
+        cin >> N >> K;
+
+        vector<int> A(N), B(N);
+        for (int i = 0; i < N; i++) {
+            cin >> A[i];
+        }
+        for (int i = 0; i < N; i++) {
+            cin >> B[i];
+        }
+        Solution obj;
+        vector<int> ans = obj.maxCombinations(N, K, A, B);
+        for (auto &it : ans) cout << it << ' ';
+        cout << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
